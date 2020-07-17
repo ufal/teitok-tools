c0e4f28379c6467a72529c97b18a5dc3fff0722e
Maarten Janssen <maartenpt@gmail.com>
2020-07-17 16:15:12 +0200

    added udpipe2teitok and manatee2teitok

---
+++ Scripts/manatee2teitok.pl
@@ -0,0 +1,183 @@
+use Encode qw(decode encode);
+use Time::HiRes qw(usleep ualarm gettimeofday tv_interval);
+use HTML::Entities;
+use XML::LibXML;
+use Getopt::Long;
+ use Data::Dumper;
+use POSIX qw(strftime);
+
+# Convert a Manatee corpus into a TEITOK corpus (to have it convert back to Manatee)
+
+$scriptname = $0;
+
+GetOptions ( ## Command line options
+            'debug' => \$debug, # debugging mode
+            'force' => \$force, # write even if project exists
+            'corpus=s' => \$corpus, # corpus to be converted
+            'regfolder=s' => \$regfolder, # registry folder
+            'corpfolder=s' => \$corpfolder, # registry folder
+            'textnode=s' => \$textnode, # registry folder
+            'makeproject' => \$makeproject, # registry folder
+            );
+
+$\ = "\n"; $, = "\t";
+
+if ( !$corpus ) { $corpus = shift; };
+if ( !$regfolder ) { $regfolder = "/opt/kontext/data/registry"; };
+if ( !$textnode ) { $textnode = "doc"; };
+if ( !$corpfolder ) { 
+	$corpfolder = $ENV{"HOME"}."/manatee2teitok"; 
+	if ( !-d $corpfolder ) { mkdir ($corpfolder); };
+};
+
+# Read the Registry file
+
+$/ =  undef;
+open FILE, "$regfolder/$corpus";
+$reg = <FILE>;
+close FILE;
+
+if ( !$reg ) { print "No such registry: $regfolder/$corpus"; exit; };
+
+if ( !$vrt && $reg =~ /VERTICAL "([^"]+)"/ ) { $vrt = $1; };
+if ( $reg =~ /NAME "([^"]+)"/ ) { $corpname = $1; }; if ( !$corpname ) { $corpname = $corpus; };
+if ( $reg =~ /PATH "([^"]+)"/ ) { $manfolder = $1; };
+
+@flds = ();
+while ( $reg =~ /\nATTRIBUTE ([^ \n]+)/g ) { 
+	$tmp = $1;
+	if ( $tmp ne 'lc' ) {
+		push(@flds, $tmp);
+	};
+};
+%sflds = ();
+while ( $reg =~ /\nSTRUCTURE ([^ ]+) \{(.+?)\}/smig ) { 
+	$lvl = $1; $tmp2 = $2; 
+	$sflds{$lvl} = ();
+	while ( $tmp2 =~ /\tATTRIBUTE ([^ \n]+)/g ) { 
+		$tmp = $1;
+		push(@{$sflds{$lvl}}, $tmp);
+	};
+};
+
+if ( !-f $vrt ) { 
+	# If the vrt file does not exist, create it from the Manatee corpus
+	`decodevert $corpus > $vrt`;
+};
+
+if ( !-f $vrt ) { print "No such VRT file: $vrt"; exit; };
+
+if ( !-d $corpfolder ) {
+	print "No such folder: $corpfolder";
+}; 
+if ( -d "$corpfolder/$corpus" && !$force ) {
+	print "Folder $corpfolder/$corpus already exists; please remove"; exit;
+};
+
+	mkdir("$corpfolder/$corpus");
+	mkdir("$corpfolder/$corpus/xmlfiles");
+	mkdir("$corpfolder/$corpus/Resources");
+
+if ( !-d "$corpfolder/$corpus" ) {
+	print "Unable to create $corpfolder/$corpus"; exit;
+};
+
+if ( $sflds{$textnode} ) { $splitting = 1; };
+
+# Build the settings file
+foreach $fld ( @flds ) {
+	$patts .= "			<item key=\"$fld\"/>\n";
+	if ( $fld eq 'word' || $fld =~ /form$/ ) {
+		$forms .= "				<item key=\"$fld\" display=\"$fld\"/>\n";
+	} else {
+		$tags .= "				<item key=\"$fld\" display=\"$fld\"/>\n";
+	};
+};
+while ( ( $key, $val ) = each ( %sflds) ) {
+	if ( $splitting && $key eq 'crp' ) { next; };
+	$satts .= "		    <item key=\"$key\" level=\"$key\" display=\"$key\">\n";
+	foreach $key2 ( @{$val} ) {
+		$satts .= "    			<item key=\"$key2\" display=\"$key2\"/>\n";
+	};
+	$satts .= "		    </item>\n";
+};
+open FILE, ">$corpfolder/$corpus/Resources/settings.xml";
+$corpid = uc($corpus);
+print FILE "<ttsettings>
+	<defaults>
+		<title display=\"$corpname\"/>
+	</defaults>
+	<cqp corpus=\"$corpid\" searchfolder=\"xmlfiles\">	
+		<pattributes>
+$patts		</pattributes>
+		<sattributes>
+$satts		</sattributes>
+	</cqp>
+
+	<xmlfile>
+		<pattributes>
+			<forms>
+$forms			</forms>
+			
+			<tags>			
+$tags			</tags>
+		</pattributes>
+		<sattributes>
+$satts
+		</sattributes>
+	</xmlfile>
+</ttsettings>";
+close FILE;
+
+$teiheader = "<teiHeader/>";
+
+if ( $splitting ) {
+	print "Creating corpus in $corpfolder/$corpus/ based on $textnode from $vrt";	
+} else {
+	print "Creating $corpfolder/$corpus/xmlfiles/fromvrt.xml from $vrt";
+	open OUTFILE, ">$corpfolder/$corpus/xmlfiles/fromvrt.xml";
+	print OUTFILE "<TEI>\n$teiheader\n<text>\n";
+};
+$/ = "\n"; $\ = undef; $glue = 0; $doccnt = 1;
+open FILE, $vrt;
+open OUTFILE, *STDOUT;
+while ( <FILE> ) {
+	chop;
+	$line = $_; 
+	if ( $splitting && $line =~ /^<\/?crp( |>)/ ) {
+		# Ignore the crp node if we are splitting files
+	} elsif ( $line =~ /^<$textnode( |>)/ ) {
+		if ( $line =~ / id="([^"]+)"/ ) {
+			$filename = $1;
+		} else { $filename = $textnode.$doccnt++; };
+		open OUTFILE, ">$corpfolder/$corpus/xmlfiles/$filename.xml";
+		print "Creating $filename.xml\n";
+		print OUTFILE "<TEI>\n$teiheader\n";
+		if ( $textnode ne 'text' ) { print OUTFILE  "<text>\n"; };
+		print OUTFILE $line."\n"; 
+	} elsif ( $line =~ /^<\/$textnode( |>)/ ) {
+		if ( $textnode ne 'text' ) { print OUTFILE $line; };
+		print OUTFILE "\n</text>\n</TEI>";	
+		close OUTFILE;
+		open OUTFILE, *STDOUT;
+	} elsif ( $line =~ /^</ ) {
+		print OUTFILE $line;
+		if ( $line eq '<g/>' ) { $glue = 1; } elsif ( $line =~ /^<(p|s) / ) { $glue = 0; }; 
+		if ( !$glue ) { print OUTFILE "\n"; };
+	} else {
+		@tmp = split ( "\t", $line ); $atts = "";
+		for ( $i=1; $i<scalar @flds; $i++ ) {
+			$atts .= " ".$flds[$i]."=\"".$tmp[$i]."\"";
+		};
+		$tok = "<tok $atts>$tmp[0]</tok>";
+		if ( !$glue ) { print OUTFILE " "; }; $glue = 0; # Print a space unless after <g/>
+		print OUTFILE $tok;
+	};
+	
+};
+close FILE;
+if ( !$splitting ) {
+	print OUTFILE "\n</text>\n</TEI>";
+};
+close OUTFILE;
+
---
+++ Scripts/udpipe2teitok.pl
@@ -0,0 +1,231 @@
+use Getopt::Long;
+ use Data::Dumper;
+use POSIX qw(strftime);
+use File::Find;
+use LWP::Simple;
+use LWP::UserAgent;
+use JSON;
+use XML::LibXML;
+
+# Convert a Manatee corpus into a TEITOK corpus (to have it convert back to Manatee)
+
+$scriptname = $0;
+
+GetOptions ( ## Command line options
+            'debug' => \$debug, # debugging mode
+            'model=s' => \$model, # which UDPIPE model to use
+            'lang=s' => \$lang, # language of the texts (if no model is provided)
+            'orgfolder=s' => \$orgfolder, # Originals folder
+            );
+
+$\ = "\n"; $, = "\t";
+
+$ua = LWP::UserAgent->new(ssl_opts => { verify_hostname => 1 });
+
+if ( !$orgfolder ) { $orgfolder = "Originals"; };
+if ( !-d $orgfolder ) { print "No original files folder $orgfolder"; exit; };
+
+( $tmp = $0 ) =~ s/Scripts.*/Resources\/udpipe-models.txt/;
+open FILE, $tmp; %udm = ();
+while ( <FILE> ) {
+	chop;
+	( $code, $lg, $mod ) = split ( "\t" );
+	$code2model{$code} = $mod;
+	$lang2model{$lg} = $mod;
+	$mod2lang{$mod} = ucfirst($lg);
+	$mod2code{$mod} = $code;
+	$models{$mod} = 1;
+}; 
+if ( !$model ) {
+	if ( $lang ) { 
+		$model = $code2model{$lang} or $model = $lang2model{lc($lang)};
+		if ( !$model ) { print "No UDPIPE models for $lang"; exit; };
+		print "Choosing $model for $lang";
+	};
+} elsif ( !$models{$model} ) { print "No such UDPIPE model: $model"; exit;  };
+
+print "Using model: $model";
+
+mkdir("udpipe");
+find({ wanted => \&treatfile, follow => 1, no_chdir => 1 }, $orgfolder);
+
+STDOUT->autoflush();
+
+sub treatfile ( $fn ) {
+	$fn = $_;  $orgfile = $fn;
+	if ( !-d $fn ) { 
+		print "\nTreating $fn";
+	
+		# read the text
+		$/ = undef;
+		open FILE, $fn;
+		binmode (FILE, ":utf8");
+		$raw = <FILE>;
+		close FILE;
+
+		( $udfile = $fn ) =~ s/$orgfolder/udpipe/;
+		$udfile =~ s/\..*?$/\.conllu/;
+		( $tmp = $udfile ) =~ s/\/[^\/]+$//;
+		`mkdir -p $tmp`;
+		$conllu = runudpipe($raw, $model);
+		print " - Writing to $udfile";
+		open FILE, ">$udfile";
+		binmode (FILE, ":utf8");
+		print FILE $conllu;
+		close FILE;
+		
+		( $xmlfile = $udfile ) =~ s/udpipe/xmlfiles/;
+		$xmlfile =~ s/\.conllu$/\.xml/;
+		( $tmp = $xmlfile ) =~ s/\/[^\/]+$//;
+		`mkdir -p $tmp`;
+		$teitext = conllu2tei($udfile);
+		$now = strftime('%Y-%m-%d', localtime());
+		$teixml = "<TEI>
+<teiHeader>
+	<fileDesc>
+		<profileDesc>
+			<langUsage><language code=\"$mod2code{$model}\">$mod2lang{$model}</language></langUsage>
+		</profileDesc>
+	</fileDesc>
+	<notesStmt><note n=\"orgfile\">$orgfile</note></notesStmt>
+	<revisionDesc><change who=\"udpipe\" when=\"$now\">dependency parsed with the udpipe web-service using model $model</change></revisionDesc>
+</teiHeader>
+<text>
+$teitext
+</text>
+</TEI>";
+		print " - Writing to $xmlfile";
+		open FILE, ">$xmlfile";
+		# binmode (FILE, ":utf8");
+		print FILE $teixml;
+		close FILE;
+		
+
+	};
+};
+
+sub runudpipe ( $raw, $model ) {
+	($raw, $model) = @_;
+
+	%form = (
+		"tokenizer" => "1",
+		"tagger" => "1",
+		"parser" => "1",
+		"model" => $model,
+		"data" => $raw
+	);
+	
+	$url = "http://lindat.mff.cuni.cz/services/udpipe/api/process";
+		print " - Running UDPIPE from $url/$model";
+	$res = $ua->post( $url, \%form );
+	$jsdat = $res->decoded_content;
+	$jsonkont = decode_json($jsdat);
+
+
+	return $jsonkont->{'result'};
+	
+};
+
+sub conllu2tei($fn) {
+	$fn = @_[0]; $tokcnt = 1; %tok = (); %mtok = (); %etok = (); %etok = (); %sent = (); $scnt=1; $mtokcnt=1; $prevdoc = "";
+	if ( $fn =~ /\/([a-z]+)_([a-z]+)-ud-([a-z]+).conllu/ ) { $lang = $1; $name = $2; $part = $3; };
+	$linex = ""; 
+
+	$/ = "\n";
+	open FILE, $fn; $insent = 0; $inpar = 0; $indoc = 0; $doccnt =1;
+	while ( <FILE> ) {	
+		$line = $_; chop($line);
+		if ( $line =~ /# newdoc id = (.*)/ || $line =~ /# newdoc/ ) {
+			if ( $inpar ) { $linex .= "</p>\n"; $inpar = 0; }; # A new document always closes the paragraph
+			if ( $indoc ) { $linex .= "</doc>\n"; $indoc = 0; }; # A new document always closes the paragraph
+			$linex .= "<doc>\n"; 
+			$indoc = $1 or $indoc = "doc$doccnt";
+			$doccnt++;
+		} elsif ( $line =~ /# newpar id = (.*)/ || $line =~ /# newpar/ ) {
+			if ( $inpar ) { $linex .= "</p>\n"; };
+			$linex .= "<p org_id=\"$1\">\n"; 
+			$inpar = 1;
+		} elsif ( $line =~ /# ?([a-z0-9A-Z\[\]¹_-]+) ?=? (.*)/ ) {
+			$sent{$1} = $2;
+		} elsif ( $line =~ /^(\d+)\t(.*)/ ) {
+			$tok{$1} = $2; $tokmax = $1; 
+			$tokid{$1} = $tokcnt++;	
+		} elsif ( $line =~ /^(\d+)-(\d+)\t(.*)/ ) {
+			# To do : mtok / dtok	
+			$mtok{$1} = $3; $etok{$2} = $3; $mtoke{$1} = $2;
+		} elsif ( $line =~ /^(\d+\.\d+)\t(.*)/ ) {
+			# To do : non-word tokens; ignore for now (extended trees - only becomes relevant if UD integration stronger)
+		} elsif ( $line =~ /^#/ ) {
+			# To do : ??	
+		} elsif ( $line eq '' ) {
+			$linex .= makesent(%sent, %tok);
+			%tok = (); %mtok = ();  %etok = ();  %tokid = ();  %sent = ();
+		} else {
+			print "What? ($line)"; 
+		};
+	};
+	if ( keys %sent ) { $linex .= makesent(%sent, %tok); }; # Add the last sentence if needed
+	if ( $inpar ) { $linex .= "</p>\n"; };
+	if ( $indoc ) { $linex .= "</doc>\n"; };
+
+	return $linex;
+		
+};
+
+sub makesent($sent, $tok) {
+	( $sent, $tok ) = @_;
+	
+	if ( !scalar $tok) { return; };
+	
+	$moresf = "";
+	while ( ( $key, $val ) = each (%sent) ) { 
+		$att = $key; $att =~ s/\[/_/g; $att =~ s/[^a-z_]//g;
+		if ( $att ne 'id' && $att ne '' ) { $moresf .= " $att=\"".textprotect($val)."\""; };
+	};
+	$sentxml = "<s id=\"s-".$scnt++."\" $moresf>"; $dtokxml = "";
+	for ( $i=1; $i<=$tokmax; $i++ ) {
+		$tokline = textprotect($tok{$i});
+		( $word, $lemma, $upos, $xpos, $feats, $head, $deprel, $deps, $misc ) = split("\t", $tokline ); 
+		if ( $head ) { $headf = "head=\"w-".$tokid{$head}."\""; };
+		if ( $mtok{$i} ) { 
+			( $mword, $mlemma, $mupos, $mxpos, $mfeats, $mhead, $mdeprel, $mdeps, $mmisc ) = split("\t", $mtok{$i}); 
+			if ( $mword =~ / / ) {
+				$sentxml .= "<mtok id=\"w-".$mtok++."\" form=\"$mword\" lemma=\"$mlemma\" upos=\"$mupos\" xpos=\"$mxpos\" feats=\"$mfeats\" deprel=\"$mdeprel\" oid=\"$i\" ohd=\"$head\" deps=\"$deps\" misc=\"$misc\" $mheadf>";			
+			} else {
+				$dtokxml = "<tok id=\"w-".$tokid{$i}."\" lemma=\"$mlemma\" upos=\"$mupos\" xpos=\"$mxpos\" feats=\"$mfeats\" deprel=\"$mdeprel\" deps=\"$mdeps\" misc=\"$mmisc\" $mheadf>$mword";			
+			};
+		}		
+		if ( $dtokxml ) {
+			$dtokxml .= "<dtok id=\"w-".$tokid{$i}."\" lemma=\"$lemma\" upos=\"$upos\" xpos=\"$xpos\" feats=\"$feats\" deprel=\"$deprel\" $headf form=\"$word\"/>";			
+		} else {
+			$tokxml = "<tok id=\"w-".$tokid{$i}."\" lemma=\"$lemma\" upos=\"$upos\" xpos=\"$xpos\" feats=\"$feats\" deprel=\"$deprel\" $headf>$word</tok>";
+			$tokxml =~ s/ [a-z]+="_"//g; # Remove empty attributes
+			$sentxml .= $tokxml;
+			if ( $misc !~ /SpaceAfter=No/ ) { $sentxml .= " "; }; # Add a space unless told not to
+		};
+		if ( $etok{$i} ) {
+			if ( $dtokxml ) {
+				$dtokxml .= "</tok>";
+				$sentxml .= $dtokxml;		
+				if ( $nsp !~ /SpaceAfter=No/ ) { $sentxml .= " "; }; # Add a space unless told not to (does that work with spans?)
+				$dtokxml = "";
+			} else {
+				$sentxml .= "</mtok>";			
+			};
+		};
+	}; 
+	$sentxml .= "</s>\n"; 
+	
+	return $sentxml;
+};
+
+sub textprotect ( $text ) {
+	$text = @_[0];
+	
+	$text =~ s/&/&amp;/g;
+	$text =~ s/</&lt;/g; 
+	$text =~ s/>/&gt;/g;
+	$text =~ s/"/&#039;/g;
+
+	return $text;
+};
