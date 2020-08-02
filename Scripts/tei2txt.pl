7c3433eb4d8ad671caf4a95f82e85cf62511a5b5
maartenpt <maartenpt@gmail.com>
2020-08-02 18:40:04 +0200

    added tei2txt

---
+++ Scripts/tei2txt.pl
@@ -0,0 +1,31 @@
+use Getopt::Long;
+use XML::LibXML;
+
+# Convert a TEI document into plain text
+
+GetOptions ( ## Command line options
+            'debug' => \$debug, # debugging mode
+            'mixed' => \$mixed, # mixed language corpus - detect for each text
+            'file=s' => \$filename, # which UDPIPE model to use
+            );
+
+$\ = "\n"; $, = "\t";
+
+if ( !$filename ) { $filename = shift; };
+
+$/ = undef;
+open FILE, $filename;
+binmode(FILE, ":utf8");
+$xml = <FILE>;
+close FILE;
+
+$xml =~ s/.*<text[^>]+>//sm;
+$xml =~ s/<\/text>.*//sm;
+
+$xml =~ s/\n+/ /g;
+$xml =~ s/<del(?=>| ).*?<\/del>//g;
+$xml =~ s/<p>/\n\n/g;
+$xml =~ s/<[^>]+>//g;
+$xml =~ s/ +/ /g;
+
+print $xml;
