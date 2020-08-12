f9b480cf0c38c713f391382017d695d7c21b4d4d
maartenpt <maartenpt@gmail.com>
2020-08-12 16:31:05 +0200

    added chat2teitok

---
+++ Scripts/chat2teitok.pl
@@ -0,0 +1,256 @@
+use utf8;
+use XML::LibXML;
+use Data::Dumper;
+use Getopt::Long;
+
+ GetOptions ( ## Command line options
+            'debug' => \$debug, # debugging mode
+            'test' => \$test, # tokenize to string, do not change the database
+            'output=s' => \$output, # name of the output file - if empty STDOUT
+            'morerev=s' => \$morerev, # More revision statements
+            'file=s' => \$filename, # filename of the input
+            'options=s' => \$options, # format of the transcription
+            );
+
+$\ = "\n"; $, = "\t";
+
+binmode STDOUT, "utf8:";
+
+if ( !$filename  ) { $filename = shift; };
+if ( !$output ) { ( $output = $filename ) =~ s/\..+?$/.xml/; };
+
+$parser = XML::LibXML->new(); $doc = "";
+$doc = XML::LibXML::Document->new(1.0, "UTF-8");
+$root = $doc->createElement("TEI");
+$doc->setDocumentElement($root);
+$teiheader = $doc->createElement("teiHeader"); $doc->firstChild->addChild($teiheader);
+$text = $doc->createElement("text"); $doc->firstChild->addChild($text);
+
+$xps{'Comment'} = ["/TEI/teiHeader/notesStmt", "note"];
+$xps{'PID'} = ["/TEI/teiHeader/fileDesc/titleStmt", "title"];
+$xps{'Date'} = ["/TEI/teiHeader/fileDesc/titleStmt", "date"];
+$xps{'Languages'} = ["/TEI/teiHeader/profileDesc/langUsage", "language", "ident"];
+$xps{'Location'} = ["/TEI/teiHeader/recordingStmt/recording", "location"];
+$xps{'Transcriber'} = ["/TEI/teiHeader/fileDesc/titleStmt/respStmt", "resp", "", "n", "Transcription"];
+$xps{'Types'} = ["/TEI/teiHeader/profileDesc/textClass/keywords", "term", "", "type", "genre"];
+
+$elms{'G'} = ["milestone", "n"];
+
+@ids = ("langKnowledge/langKnown[\@level=\"first\"]","*name","*id","*??","\@sex","*","*","*role","*","*");
+
+$convs{'CA'} = "CLAN";
+$convs{'heritage'} = "Heritage";
+
+$/ = undef;	
+open FILE, $filename;
+binmode  FILE, "utf8:";
+$rawtext = <FILE>;
+close FILE;
+
+$rawtext =~ s/\n\s+(?!\(\d)/ /g;
+
+foreach $line  ( split ( "\n", $rawtext ) ) {
+	$line =~ s/[\r\n]//g; $line =~ s/\0//g;
+
+	if ( $line =~ /@([^:]+):\s*(.*)/ ) {
+		# Metadata line	
+		$fld = $1; $val = $2;
+		if ( $fld eq 'Options' ) {
+			if ( !$transform ) { $transform = $val; };
+		} elsif ( $fld eq 'Participants' ) {
+			$basenode = makenode($doc, "/TEI/teiHeader/profileDesc/particDesc/listPerson");
+			foreach $pfld ( split ( ", ", $val ) ) {
+				( $code, $name, $role ) = split(" ", $pfld);
+				$person{$code} = $doc->createElement("person"); 
+				$person{$code}->setAttribute("id", $code);
+				$person{$code}->setAttribute("role", $role);
+				$person{$code}->appendText($name);
+				$basenode->addChild($person{$code});
+			};
+		} elsif ( $fld eq 'ID' ) {
+			@flds = split("[|]", $val);
+			if ( !$person{$flds[2]} ) { print "No such person in $val: ".$flds[2]; exit; };
+			for ( $i=0; $i<scalar @flds; $i++ ) { 
+				if ( $flds[$i] && substr($ids[$i],0,1) ne "*" ) {
+					$node = makenode($person{$flds[2]}, "./".$ids[$i]);
+					if ( $node->nodeType == 2 ) {
+						$node->parentNode->setAttribute($node->getName(), $flds[$i]);
+					} else {
+						$node->appendText($flds[$i]);
+					};
+				};
+			};
+		} elsif ( $fld eq 'Media' ) {
+			($recurl, $type) = split(", ", $val);
+			$media = makenode($doc, "/TEI/teiHeader/recordingStmt/recording/media");
+			if ( $recurl !~ /\./ ) {
+				if ( $type eq "audio" ) { 
+					$recurl .= ".mp3"; 
+					$media->setAttribute("mimeType", "audio/mp3");
+				} elsif ( $type eq "video" ) { 
+					$recurl .= ".mpg"; 
+					$media->setAttribute("mimeType", "video/mpg");
+				};
+			};
+			$media->setAttribute("url", $recurl);
+		} elsif ( $xps{$fld} ) {
+			($base, $elm, $att, $satt, $sval) = @{$xps{$fld}};
+			if ( $intext ) { 
+				$satt = $att or $satt = $elm;
+				if ( !$milestone ) {
+					$milestone = $doc->createElement("milestone"); $text->addChild($milestone);
+				};
+				if ( $milestone->getAttribute($satt) ) { $val = $milestone->getAttribute($satt)."; ".$val};
+				$milestone->setAttribute(lc($satt), $val);
+			} else {
+				utf8::downgrade($val);
+				$basenode = makenode($doc, $base);
+				$valnode = $doc->createElement($elm); 
+				$basenode->addChild($valnode);
+				if ( $satt ) {
+					$valnode->setAttribute($satt, $sval);
+				};
+				if ( $att ) {
+					$valnode->setAttribute($att, $val);
+				} else {
+					$valnode->appendText($val);
+				};
+			};
+		} elsif ( $elms{$fld} ) {
+			($elm, $att) = @{$elms{$fld}};
+			$milestone = $doc->createElement($elm); $text->addChild($milestone);
+			$milestone->setAttribute($att, $val);
+			$intext = 1;
+		} else {
+			# Print unknown field
+			$basenode = makenode($doc, "/TEI/teiHeader/notesStmt");
+			$valnode = $doc->createElement("note"); 
+			$valnode->setAttribute("n", $fld);
+			$valnode->appendText($val);
+			$basenode->addChild($valnode);
+		};
+	} elsif ( $line =~ /\*([^:]+):\s*(.*)/ ) {
+		# Transcription line
+		$who = $1; $trans = $2; $intext = 1;
+		if ( $convs{$options} ) {
+			$transxml = convutt($trans, $options);
+			$uttxml = $parser->load_xml(string => $transxml, { no_blanks => 1 });
+			$utt = $uttxml->firstChild;
+			$text->addChild($utt);
+		} else {
+			$utt = $doc->createElement("u"); $text->addChild($utt);
+			$utt->appendText($trans);
+		};
+			$utt->setAttribute("who", $who);
+	} elsif ( $line =~ /\%([^:]+):\s*(.*)/ ) {
+		# Transcription line
+		$channel = $1; $val = $2; 
+		$note = $doc->createElement("note"); $text->addChild($note);
+		$note->appendText($val);
+		$note->setAttribute("n", $channel);
+	} else {
+		# Continuation of a transcription line?
+		if ( $debug ) {
+			print "Line? $line";
+		};
+	};
+	
+};
+
+
+$basename = $filename;
+$basename =~ s/.*\///;
+$basename =~ s/\.[^.]+$//;
+
+print "Writing output to $output";
+open OUTFILE, ">$output";
+print OUTFILE $doc->toString(1);
+close OUTFILE;
+
+sub convutt ( $trans, $format ) {
+	( $trans, $format ) = @_;
+
+	# Protect any codes for XML
+	$trans =~ s/&/&amp;/g;
+	$trans =~ s/</&lt;/g;
+	$trans =~ s/>/&gt;/g;
+	
+	if ( $format eq 'CA' ) {
+	
+		# print $trans;
+		if ( $trans =~ s/(\d+)_(\d+)// ) {
+			$begin = ($1/1000); $end = ($2/1000);
+			$timing = " begin=\"$begin\" end=\"$end\"";  
+		};
+	
+	} elsif ( $format eq 'heritage' ) {
+		$trans =~ s/&lt;(.*?)&gt;/<del reason="reformulation">\1<\/del>/g;
+		$trans =~ s/&amp;([^ <>]+)/<del reason="truncation">\1<\/del>/g;
+
+		$trans =~ s/\[\/\]/<pause type="short"\/>/g;
+		$trans =~ s/\[\/\/\]/<pause type="long"\/>/g;
+
+	}; 
+	
+	
+	return "<u$timing>$trans</u>";
+};
+
+sub in_array ( $check, @list ) {
+	$check = $_[0]; @list = @{$_[1]};
+	
+	foreach $elm ( @list ) {
+		if ( $elm eq $check ) {
+			return 1;
+		};
+	};
+	
+	return 0;
+}
+
+sub makenode ( $xml, $xquery ) {
+	my ( $xml, $xquery ) = @_;
+	$tmp = $xml->findnodes($xquery); 
+	if ( $tmp ) { 
+		if ( $debug ) { print "Node exists: $xquery"; };
+		return $tmp->item(0);
+	} else {
+		if ( $xquery =~ /^(.*)\/(.*?)$/ ) {
+			my $parxp = $1; my $thisname = $2;
+			my $parnode = makenode($xml, $parxp);
+			$thisatts = "";
+			if ( $thisname =~ /^(.*)\[(.*?)\]$/ ) {
+				$thisname = $1; $thisatts = $2;
+			};
+			if ( $thisname =~ /^@(.*)/ ) {
+				$attname = $1;
+				$parnode->setAttribute($attname, '');
+				foreach $att ( $parnode->attributes() ) {
+					if ( $att->getName eq $attname ) {
+						return $att;
+					};
+				};
+			} else {
+				$newchild = XML::LibXML::Element->new( $thisname );
+			
+				# Set any attributes defined for this node
+				if ( $thisatts ne '' ) {
+					if ( $debug ) { print "setting attributes $thisatts"; };
+					foreach $ap ( split ( " and ", $thisatts ) ) {
+						if ( $ap =~ /\@([^ ]+) *= *"(.*?)"/ ) {
+							$an = $1; $av = $2; 
+							$newchild->setAttribute($an, $av);
+						};
+					};
+				};
+
+				if ( $debug ) { print "Creating node: $xquery ($thisname)"; };
+				$parnode->addChild($newchild);
+			};
+			
+		} else {
+			print "Failed to find or create node: $xquery";
+		};
+	};
+};
+
---
+++ Scripts/runkorektor.pl
@@ -0,0 +1,198 @@
+use Getopt::Long;
+use Data::Dumper;
+use POSIX qw(strftime);
+use File::Find;
+use LWP::Simple;
+use LWP::UserAgent;
+use JSON;
+use XML::LibXML;
+use Encode;
+
+# Parse a tokenized corpus using UDPIPE
+
+$scriptname = $0;
+
+GetOptions ( ## Command line options
+            'debug' => \$debug, # debugging mode
+            'file=s' => \$filename, # which UDPIPE model to use
+            'model=s' => \$model, # which UDPIPE model to use
+            'folder=s' => \$folder, # Originals folder
+            'token=s' => \$token, # token node
+            'tokxp=s' => \$tokxp, # token XPath
+            );
+
+$\ = "\n"; $, = "\t";
+
+$ua = LWP::UserAgent->new(ssl_opts => { verify_hostname => 1 });
+$parser = XML::LibXML->new(); 
+
+if ( !$filename ) { $filename = shift; };
+if ( !$token ) { $token = "tok"; };
+if ( !$model ) { $model = "czech-spellchecker-130202"; };
+
+binmode(STDOUT, ":utf8");
+
+print "Using model: $model";
+
+
+if ( $filename ) {
+	if ( !-e $filename ) { print "No such file: $file"; exit; };
+	treatfile($filename);
+} elsif ( $folder ) { 
+	if ( !-d $folder ) { print "No such folder: $folder"; exit; };
+	find({ wanted => \&treatfile, follow => 1, no_chdir => 1 }, $folder);
+} else {
+	print "Please provide a file or folder to parse"; exit;
+};
+
+
+STDOUT->autoflush();
+
+sub treatfile ( $fn ) {
+	$tokcnt = 1;
+	$fn = $_; if ( !$fn ) { $fn = @_[0]; }; $orgfile = $fn;
+	if ( !-d $fn ) { 
+		print "\nTreating $fn";
+	
+		# read the XML
+		eval {
+			$xml = $parser->load_xml(location => $fn, load_ext_dtd => 0);
+		};
+		if ( !$xml ) { 
+			print "Invalid XML in $fn";
+			continue;
+		};
+		if ( !$tokxp ) { $tokxp = "//$token"; };
+		$num = 1; 
+
+		$toklist = "";
+		foreach $tok ( $xml->findnodes($tokxp) ) {
+			if ( $newsent ) { $toklist .= "\n"; };
+			$newsent = 0;
+			$tokid = $tok->getAttribute('id');
+			if ( !$tokid ) { $tokid = "w-".$tokcnt++; $tok->setAttribute('id', $tokid); };
+			$form = $tok->getAttribute("form");
+			if ( !$form ) { $form = $tok->textContent; };
+			if ( !$form ) { $form = "_"; };	
+			push(@tokarray, [$form, $tokid]);
+			$tokhash{$tokid} = $tok;
+			$toklist .= $form." ";
+		};
+		utf8::upgrade($toklist);
+		
+		if ( $debug ) { 
+			binmode(STDOUT, ":utf8");
+			print $toklist; 
+		};
+				
+		$udfile = $fn;
+		if ( $folder eq '' ) { $udfile = "udpipe/$udfile"; };
+		$udfile =~ s/\..*?$/\.sug/;
+		( $tmp = $udfile ) =~ s/\/[^\/]+$//;
+		$response = runkorektor($toklist, $model);
+
+		foreach $span ( @{$response} ) {
+			@tmp = @{$span};
+			$words = $tmp[0];
+			utf8::upgrade($words);
+			if ( scalar @tmp == 1 ) { 
+				# A span of non-corrected words
+				foreach $word ( split(" ", $words) ) {
+					if ( $tokarray[0][0] eq $word ) { shift(@tokarray); } else { print "Oops: $word =/= ".$tokarray[0][0]; exit; };
+				};
+			} else {
+				# A correction
+				$corr = $tmp[1];
+				if ( $tokarray[0][0] eq $words ) { 
+					$todoid = $tokarray[0][1];
+					shift(@tokarray); 
+					$tokhash{$todoid}->setAttribute('nform', $corr);
+					print "    - corrected $tokid from $words to $corr";
+				} else { print "Oops: $words =/= ".$tokarray[0][0]; exit; };
+			};
+		};
+		
+		# Add the revision statement
+		$revnode = makenode($xml, "/TEI/teiHeader/revisionDesc/change[\@who=\"korektor\"]");
+		$when = strftime "%Y-%m-%d", localtime;
+		$revnode->setAttribute("when", $when);
+		$revnode->appendText("Spell-checked with Korektor using $model");
+		
+		$outfile = $orgfile;
+		print "Writing parsed file to $outfile\n";
+		open OUTFILE, ">$outfile";
+		print OUTFILE $xml->toString;	
+		close OUTFLE;
+		
+	};
+};
+
+
+sub runkorektor ( $raw, $model ) {
+	($raw, $model) = @_;
+
+	%form = (
+		"input" => "horizontal",
+		"model" => $model,
+		"data" => $raw,
+	);
+	
+	$url = "http://lindat.mff.cuni.cz/services/korektor/api/suggestions";
+		print " - Running korektor from $url / $model";
+	$res = $ua->post( $url, \%form );
+	$jsdat = $res->decoded_content;
+	$jsonkont = decode_json($res->decoded_content);
+
+	return $jsonkont->{'result'};
+	
+};
+
+
+sub textprotect ( $text ) {
+	$text = @_[0];
+	
+	$text =~ s/&/&amp;/g;
+	$text =~ s/</&lt;/g; 
+	$text =~ s/>/&gt;/g;
+	$text =~ s/"/&#039;/g;
+
+	return $text;
+};
+
+sub makenode ( $xml, $xquery ) {
+	my ( $xml, $xquery ) = @_;
+	@tmp = $xml->findnodes($xquery); 
+	if ( scalar @tmp ) { 
+		$node = shift(@tmp);
+		if ( $debug ) { print "Node exists: $xquery"; };
+		return $node;
+	} else {
+		if ( $xquery =~ /^(.*)\/(.*?)$/ ) {
+			my $parxp = $1; my $thisname = $2;
+			my $parnode = makenode($xml, $parxp);
+			$thisatts = "";
+			if ( $thisname =~ /^(.*)\[(.*?)\]$/ ) {
+				$thisname = $1; $thisatts = $2;
+			};
+			$newchild = XML::LibXML::Element->new( $thisname );
+			
+			# Set any attributes defined for this node
+			if ( $thisatts ne '' ) {
+				if ( $debug ) { print "setting attributes $thisatts"; };
+				foreach $ap ( split ( " and ", $thisatts ) ) {
+					if ( $ap =~ /\@([^ ]+) *= *"(.*?)"/ ) {
+						$an = $1; $av = $2; 
+						$newchild->setAttribute($an, $av);
+					};
+				};
+			};
+
+			if ( $debug ) { print "Creating node: $xquery ($thisname)"; };
+			$parnode->addChild($newchild);
+			
+		} else {
+			print "Failed to find or create node: $xquery";
+		};
+	};
+};
+
