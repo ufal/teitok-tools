61a05997df604c496b9c0bea4d06275fdf5a4a57
maartenpt <maartenpt@gmail.com>
2020-08-18 13:42:18 +0200

    addded brat and conllu2teitok

---
+++ Scripts/brat2teitok.pl
@@ -0,0 +1,175 @@
+use Encode;
+use utf8;
+use Getopt::Long;
+use XML::LibXML;
+
+# Script to convert an EXMARaLDA text into TEITOK/XML
+
+ GetOptions ( ## Command line options
+            'debug' => \$debug, # debugging mode
+            'file=s' => \$filename, # language of input
+            'plain=s' => \$plainfile, # location of the plain text file
+            'morerev=s' => \$morerev, # more revision statement
+            'annfolder=s' => \$annfolder, # folder to write annotation to
+            'output=s' => \$outfolder, # folder to write the brat file to
+            );
+
+$/ = undef; $\ = "\n"; $, = "\t";
+
+if ( !$filename ) { $filename = shift; };
+if ( !-e $filename && $filename !~ /\.ann$/ ) { $filename .= ".ann"; };
+if ( !-e $filename ) { print "No such file: $filename"; };
+( $basename = $filename ) =~ s/\.[^.]+$//; $basename =~ s/.*\///;
+if ( !$plainfile ) { ( $plainfile = $filename ) =~ s/\.ann$/.txt/; };
+if ( !$outfile ) { ( $outfolder = $filename ) =~ s/[^\/]+$//;if ( !$outfolder ) { $outfolder = ".";}; };
+
+$/ = undef;
+# binmode(STDOUT, ":utf8");
+
+open FILE, $plainfile;
+binmode(FILE, ":utf8");
+$text = <FILE>;
+close FILE;
+
+if ( $text eq "" ) { print "Text not read."; exit; }
+
+$\ = "\n"; $, = "\t";
+
+$ws = 0; $ss = 0; $org = 1; $toks = ""; $cnt = 1;
+for $i (0..length($text)-1){
+    $char = substr($text, $i, 1);
+    # print "Index: $i, Text: $char \n";
+    		
+	$mapto{$i} = $cnt;
+    if ( ( $char eq " " || $char eq "\n" ) && $org ) {
+    	$we = $i; $word = substr($text, $ws, $we-$ws);
+		$tokcnt = $cnt;
+		
+		# Split off left punctuations
+		$befp = ""; 
+		while ( $word =~ /^(\p{isPunct})(.+)/ ) {
+			$punct = $1; $word = $2;
+			$begins{$tokcnt} = $ws; $ends{$tokcnt} = $ws;
+			$mapto{$ws} = $tokcnt;
+			$toktxt{$tokcnt} = $punct;
+    		$befp .= "<tok id=\"w-$tokcnt\" idx=\"$ws-$ws\">$punct</tok>";
+			$ws++; $tokcnt++; $cnt++;
+		};
+
+		# Split off right punctuations
+		$aftp = "";
+		while ( $word =~ /(.+)(\p{isPunct})+/ ) {
+			$punct = $2; $word = $1;
+			$pcnt = $tokcnt + 1;
+			$begins{$pcnt} = $we; $ends{$pcnt} = $we;
+			$toktxt{$pcnt} = $punct;
+			$mapto{$we} = $pcnt;
+    		$aftp .= "<tok id=\"w-$pcnt\" idx=\"$we-$we\">$punct</tok>";
+			$we--; $cnt++;
+		};
+
+		for ( $j=$ws; $j<$we+1; $j++ ) { $mapto{$j} = $tokcnt; };
+	
+    	$mf= ""; if ( $word =~ /\*/ ) { $mf = " mf=\"$word\""; $word =~ s/\*//g; };
+    	$begins{$tokcnt} = $ws; $ends{$tokcnt} = $we;
+    	$toktxt{$tokcnt} = $word;
+    	$toks .= "$befp<tok id=\"w-$tokcnt\" idx=\"$ws-$we\"$mf>$word</tok>$aftp$char";
+    	$ws = $i+1;
+    	$cnt++;
+    };
+    if ( $makesent && $char eq "\n" ) {
+    	$se = $i;
+    	# print "Phrase ($org): ".substr( $text, $ss, $se-$ss );
+    	if ( $org == 0 ) {
+    		$ths = substr( $text, $ss, $se-$ss );
+    		$toks =~ s/ $//;
+    		$sent = "<s th=\"$ths\">".$toks."</s>\n";
+    		$sent =~ s/ ([,.!?)])/\1/g;
+    		$sent =~ s/([(]) /\1/g;
+    		# print $sent;
+    		$tei .= $sent;
+	    	$toks = "";
+    	};
+    	$ss = $i+1; $ws = $i+1;
+    	$org = 1-$org;
+    };
+           
+}
+if ( !$makesent ) { $tei = $toks; 	};
+
+$tei =~ s/ (<tok[^>]+>[,.!?)])/\1/g;
+$tei =~ s/([(]<\/tok>) /\1/g;
+
+$tei = "<TEI>
+<teiHeader/>
+<text>
+$tei</text>
+$annotation</TEI>";
+
+my $xml = XML::LibXML->load_xml(
+	string => $tei,
+); if ( !$xml ) { print FILE "Not able to parse generated TEI"; exit; };
+
+$root = $xml->findnodes("//text")->item(0);
+
+if ( $annfolder ) {
+	my $annxml = XML::LibXML->load_xml(
+		string => "<spanGrp/>",
+	); if ( !$annxml ) { print FILE "Not able to parse annotation"; exit; };
+	$spans = $xml->findnodes("//text")->item(0);
+} else {
+	$spans = $xml->createElement("spanGrp");
+	$root->addChild($spans);
+};
+
+
+$/ = "\n"; 
+open FILE, "$filename";
+binmode ( FILE, ":utf8" ); $cnt=1;
+while (<FILE>) {
+	chomp; $line = $_;
+	if ( /(.*?)\s+(.*?)\s+(.*?)\s+(.*?)\s+(.*)/ ) { $id = $1; $type = $2; $begin = $3; $end = $4; $th = $5; };
+	if ( $line =~ /^#/ ) { next; };
+	
+	if ( $debug ) { print "$type: $begin-$end => $th"; };
+
+	$sep = ""; $span = ""; $spantext = "";
+	for ( $i=$mapto{$begin}; $i<$mapto{$end}+1; $i++ ) {
+		$span .= $sep."#w-".$i; 
+		$spantext .= $sep.$toktxt{$i};
+		$sep = " ";
+	};
+	
+	# Deal with word-internal annotations
+	$posi = "";
+	
+	if ( $th ne $spantext ) { print "Oops: $th ne $spantext"; };
+
+	if ( $debug ) { print "  -- found at $mapto{$begin} - $mapto{$end} = $span = $spantext"; };
+	$annotation = "<span range=\"$begin-$end\" corresp=\"$span\" code=\"$type\" $auto id=\"an-".$cnt++."\"$posi>".$th."</span>\n";
+
+	my $annnode = XML::LibXML->load_xml(
+		string => $annotation,
+	); if ( !$annnode ) { print FILE "Not able to parse annotation"; exit; };
+	$spans->addChild($annnode->firstChild);
+	$spans->appendText("\n");
+
+};
+close FILE;
+
+open FILE, ">$outfolder/$basename.xml";
+# binmode(FILE, ":utf8");
+print FILE $xml->toString;
+close FILE;
+print "Wrote xml file to $outfolder/$basename.xml";
+
+if ( $annfolder ) {
+	open FILE, ">$annfolder/brat_$basename.xml";
+	#binmode(FILE, ":utf8");
+	print FILE $xml->toString;
+	close FILE;
+	print "Wrote annotation file to $outfolder/$basename.xml";
+};
+
+
+# print `perl Scripts/combinebrat.pl $basename`;
---
+++ Scripts/conllu2teitok.pl
@@ -0,0 +1,138 @@
+use Getopt::Long;
+
+# Pars# Convert the known TEITOK differences to "pure" TEI/P5
+
+$scriptname = $0;
+
+GetOptions ( ## Command line options
+            'debug' => \$debug, # debugging mode
+            'file=s' => \$filename, # which UDPIPE model to use
+            'output=s' => \$output, # which UDPIPE model to use
+            'morerev=s' => \$morerev, # language of input
+            'split' => \$split, # Split into 1 file per language
+            );
+
+$\ = "\n"; $, = "\t";
+
+if ( !$filename ) { $filename = shift; };
+( $basename = $filename ) =~ s/.*\///; $basename =~ s/\..*//;
+if ( !$output ) { $output = $basename.".xml"; };
+
+$teixml = conllu2tei($filename);
+
+print "Writing output to $output";
+open OUTFILE, ">$output";
+print OUTFILE "<TEI>
+<teiHeader>
+	<notesStmt><note n=\"orgfile\">$basename.conllu</note></notesStmt>
+	<revisionDesc><change who=\"conllu2teitok\" when=\"$now\">Converted from CoNLL-U $filename</change></revisionDesc>
+</teiHeader>
+<text>
+$teixml
+</text>
+</TEI>";
+close OUTFILE;
+
+sub conllu2tei($fn) {
+	$fn = @_[0]; $tokcnt = 1; %tok = (); %mtok = (); %etok = (); %etok = (); %sent = (); $scnt=1; $mtokcnt=1; $prevdoc = "";
+	if ( $fn =~ /\/([a-z]+)_([a-z]+)-ud-([a-z]+).conllu/ ) { $lang = $1; $name = $2; $part = $3; };
+	$linex = ""; 
+
+	$/ = "\n";
+	open FILE, $fn; $insent = 0; $inpar = 0; $indoc = 0; $doccnt =1;
+	while ( <FILE> ) {	
+		$line = $_; chop($line);
+		if ( $line =~ /# newdoc id = (.*)/ || $line =~ /# newdoc/ ) {
+			if ( $inpar ) { $linex .= "</p>\n"; $inpar = 0; }; # A new document always closes the paragraph
+			if ( $indoc ) { $linex .= "</doc>\n"; $indoc = 0; }; # A new document always closes the paragraph
+			$linex .= "<doc>\n"; 
+			$indoc = $1 or $indoc = "doc$doccnt";
+			$doccnt++;
+		} elsif ( $line =~ /# newpar id = (.*)/ || $line =~ /# newpar/ ) {
+			if ( $inpar ) { $linex .= "</p>\n"; };
+			$linex .= "<p org_id=\"$1\">\n"; 
+			$inpar = 1;
+		} elsif ( $line =~ /# ?([a-z0-9A-Z\[\]¹_-]+) ?=? (.*)/ ) {
+			$sent{$1} = $2;
+		} elsif ( $line =~ /^(\d+)\t(.*)/ ) {
+			$tok{$1} = $2; $tokmax = $1; 
+			$tokid{$1} = $tokcnt++;	
+		} elsif ( $line =~ /^(\d+)-(\d+)\t(.*)/ ) {
+			# To do : mtok / dtok	
+			$mtok{$1} = $3; $etok{$2} = $3; $mtoke{$1} = $2;
+		} elsif ( $line =~ /^(\d+\.\d+)\t(.*)/ ) {
+			# To do : non-word tokens; ignore for now (extended trees - only becomes relevant if UD integration stronger)
+		} elsif ( $line =~ /^#/ ) {
+			# To do : ??	
+		} elsif ( $line eq '' ) {
+			$linex .= makesent(%sent, %tok);
+			%tok = (); %mtok = ();  %etok = ();  %tokid = ();  %sent = ();
+		} else {
+			print "What? ($line)"; 
+		};
+	};
+	if ( keys %sent ) { $linex .= makesent(%sent, %tok); }; # Add the last sentence if needed
+	if ( $inpar ) { $linex .= "</p>\n"; };
+	if ( $indoc ) { $linex .= "</doc>\n"; };
+
+	return $linex;
+		
+};
+
+sub makesent($sent, $tok) {
+	( $sent, $tok ) = @_;
+	
+	if ( !scalar $tok) { return; };
+	
+	$moresf = "";
+	while ( ( $key, $val ) = each (%sent) ) { 
+		$att = $key; $att =~ s/\[/_/g; $att =~ s/[^a-z_]//g;
+		if ( $att ne 'id' && $att ne '' ) { $moresf .= " $att=\"".textprotect($val)."\""; };
+	};
+	$sentxml = "<s id=\"s-".$scnt++."\" $moresf>"; $dtokxml = "";
+	for ( $i=1; $i<=$tokmax; $i++ ) {
+		$tokline = textprotect($tok{$i});
+		( $word, $lemma, $upos, $xpos, $feats, $head, $deprel, $deps, $misc ) = split("\t", $tokline ); 
+		if ( $head ) { $headf = "head=\"w-".$tokid{$head}."\""; };
+		if ( $mtok{$i} ) { 
+			( $mword, $mlemma, $mupos, $mxpos, $mfeats, $mhead, $mdeprel, $mdeps, $mmisc ) = split("\t", $mtok{$i}); 
+			if ( $mword =~ / / ) {
+				$sentxml .= "<mtok id=\"w-".$mtok++."\" form=\"$mword\" lemma=\"$mlemma\" upos=\"$mupos\" xpos=\"$mxpos\" feats=\"$mfeats\" deprel=\"$mdeprel\" oid=\"$i\" ohd=\"$head\" deps=\"$deps\" misc=\"$misc\" $mheadf>";			
+			} else {
+				$dtokxml = "<tok id=\"w-".$tokid{$i}."\" lemma=\"$mlemma\" upos=\"$mupos\" xpos=\"$mxpos\" feats=\"$mfeats\" deprel=\"$mdeprel\" deps=\"$mdeps\" misc=\"$mmisc\" $mheadf>$mword";			
+			};
+		}		
+		if ( $dtokxml ) {
+			$dtokxml .= "<dtok id=\"w-".$tokid{$i}."\" lemma=\"$lemma\" upos=\"$upos\" xpos=\"$xpos\" feats=\"$feats\" deprel=\"$deprel\" $headf form=\"$word\"/>";			
+		} else {
+			$tokxml = "<tok id=\"w-".$tokid{$i}."\" lemma=\"$lemma\" upos=\"$upos\" xpos=\"$xpos\" feats=\"$feats\" deprel=\"$deprel\" $headf>$word</tok>";
+			$tokxml =~ s/ [a-z]+="_"//g; # Remove empty attributes
+			$sentxml .= $tokxml;
+			if ( $misc !~ /SpaceAfter=No/ ) { $sentxml .= " "; }; # Add a space unless told not to
+		};
+		if ( $etok{$i} ) {
+			if ( $dtokxml ) {
+				$dtokxml .= "</tok>";
+				$sentxml .= $dtokxml;		
+				if ( $nsp !~ /SpaceAfter=No/ ) { $sentxml .= " "; }; # Add a space unless told not to (does that work with spans?)
+				$dtokxml = "";
+			} else {
+				$sentxml .= "</mtok>";			
+			};
+		};
+	}; 
+	$sentxml .= "</s>\n"; 
+	
+	return $sentxml;
+};
+
+sub textprotect ( $text ) {
+	$text = @_[0];
+	
+	$text =~ s/&/&amp;/g;
+	$text =~ s/</&lt;/g; 
+	$text =~ s/>/&gt;/g;
+	$text =~ s/"/&#039;/g;
+
+	return $text;
+};
