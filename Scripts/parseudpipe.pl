d5dee5c0bb2969d99c97f8ff9c3efd286c9b168c
maartenpt <maartenpt@gmail.com>
2021-10-08 01:02:38 +0200

    corrected head ID error in parseudpipe

--- Scripts/parseudpipe.pl
+++ Scripts/parseudpipe.pl
@@ -339,7 +339,7 @@ sub parseconllu($fn) {
 		} elsif ( $line =~ /^(\d+)\t(.*)/ ) {
 			$tmp = split ("\t", $line);
 			$tok{$1} = $2; $tokmax = $1; 
-			$ord2id{$1} = $line[9];	
+			$ord2id{$1} = $tmp[9];	
 		} elsif ( $line =~ /^(\d+)-(\d+)\t(.*)/ ) {
 			# To do : mtok / dtok	
 			$mtok{$1} = $3; $etok{$2} = $3; $mtoke{$1} = $2;
