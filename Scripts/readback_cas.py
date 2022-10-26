f02d0918cfa5b53100d8135ae28d6977ec5c304d
maartenpt <maartenpt@gmail.com>
2022-10-27 00:36:59 +0200

    added NER and chunks to readback_cas

--- Scripts/readback_cas.py
+++ Scripts/readback_cas.py
@@ -33,10 +33,30 @@ def readback(filename):
 	toks = {}
 	tokfrom = {}
 	tokto = {}
+	id2idx = {}
 	docend = -1
-	for tok in xmlf.findall("//text//tok"):
+
+	# Deal with NameSpace if needed
+	xmlns = xmlf.getroot().nsmap
+	if None in xmlns.keys() and not "tei" in xmlns.keys():
+		xmlns["tei"] = xmlns[None]
+	if "tokxp" in cargs.keys():
+		tokxp = cargs['tokxp']
+		tokres = xmlf.findall(tokxp, xmlns)
+	elif xmlns:
+		tokxp = "//tei:text//tei:tok"
+		tokres = xmlf.findall(tokxp, xmlns)
+	else:
+		tokxp = "//text//tok"
+		tokres = xmlf.findall(tokxp)
+
+	tokcnt = 0
+	for tok in tokres:
 		tokid = tok.attrib['id']
 		toks[tokid] = tok
+		id2idx[tokid] = tokcnt
+		tokcnt = tokcnt+1 
+	idlist = list(toks.keys())
 
 	with open(infile, 'rb') as f:
 	   cas = load_cas_from_xmi(f, typesystem=typesystem)
@@ -83,7 +103,24 @@ def readback(filename):
 		if "debug" in cargs.keys():
 			print("Dependency " + id1 + " /" + deprel + "/ " + id2 + " -> " + etree.tostring(toks[id1], encoding='unicode', method='xml'))
 
+		
+	# Find the highest ANN number	
+	annid = 1;
+	spans = xmlf.findall("//{*}span")
+	if spans is not None:
+		for span in spans:
+			spanid = span.get('id')
+			if spanid[0:4] == "ann-":
+				thisid = int(spanid[4:])
+				if thisid >= annid:
+					annid = thisid + 1
+	
 	# Read back the Chunks
+	chunkxml = xmlf.find("//{*}spanGrp[@type=\"chunks\"]")
+	if not chunkxml:
+		chunkxml = etree.Element("spanGrp")
+		chunkxml.set("type", "chunks")	
+	firstann = annid
 	for chunk in cas.select('de.tudarmstadt.ukp.dkpro.core.api.syntax.type.chunk.Chunk'):
 		# Extend to full tokens
 		p1 = chunk.begin
@@ -94,10 +131,30 @@ def readback(filename):
 			p2 = p2 + 1 
 		tok1 = tokfrom[p1]
 		tok2 = tokto[p2]
+		idspan = idlist[id2idx[tok1]:id2idx[tok2]+1]
+		corresp = "#" + " #".join(idspan)
 		if "debug" in cargs.keys():
 			print("Chunk " + tok1 + " - " + tok2 + ": " + chunk.chunkValue)
+		annelm = chunkxml.find(".//{*}span[@corresp=\""+corresp+"\"]")
+		if annelm is None:
+			annelm = etree.Element("span")
+			annelm.set("id", "ann-"+str(annid))
+			annelm.tail = "\n"
+			chunkxml.append(annelm)
+		annid = annid + 1
+		annelm.set("value", chunk.chunkValue)
+		annelm.set("corresp", corresp)
+	if "debug" in cargs.keys():
+		print(etree.tostring(chunkxml, pretty_print=True, encoding='unicode', method='xml'))
+	if annid > firstann and chunkxml.getparent() is None:
+		xmlf.getroot().append(chunkxml)
 
 	# Read back the Named Entities
+	nerxml = xmlf.find("//{*}spanGrp[@type=\"entities\"]")
+	firstann = annid
+	if nerxml is None:
+		nerxml = etree.Element("spanGrp")
+		nerxml.set("type", "entities")	
 	for ner in cas.select('de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity'):
 		# Extend to full tokens
 		p1 = ner.begin
@@ -108,8 +165,23 @@ def readback(filename):
 			p2 = p2 + 1 
 		tok1 = tokfrom[p1]
 		tok2 = tokto[p2]
+		idspan = idlist[id2idx[tok1]:id2idx[tok2]+1]
+		corresp = "#" + " #".join(idspan)
 		if "debug" in cargs.keys():
 			print("Named Entities " + tok1 + " - " + tok2 + ": " + ner.value)
+		annelm = nerxml.find(".//{*}span[@corresp=\""+corresp+"\"]")
+		if annelm is None:
+			annelm = etree.Element("span")
+			annelm.set("id", "ann-"+str(annid))
+			annid = annid + 1
+			annelm.tail = "\n"
+			nerxml.append(annelm)
+		annelm.set("type", ner.value)
+		annelm.set("corresp", corresp)
+	if "debug" in cargs.keys():
+		print(etree.tostring(nerxml, pretty_print=True, encoding='unicode', method='xml'))
+	if annid > firstann and nerxml.getparent() is None:
+		xmlf.getroot().append(nerxml)
 
 	if "test" in cargs.keys():
 		print(etree.tostring(xmlf, pretty_print=True, encoding='unicode', method='xml'))
