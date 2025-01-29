cb4730f0a0b34dd999ec8cda5bd5ce421f8b6dfa
maartenpt@gmail.com <maartenpt@gmail.com>
2025-01-29 15:07:18 +0100

    added phonemize

---
+++ Scripts/phonemize.py
@@ -0,0 +1,33 @@
+from phonemizer.backend import EspeakBackend
+from phonemizer.punctuation import Punctuation
+from phonemizer.separator import Separator
+import argparse
+import lxml.etree as etree
+import sys, os, string
+
+parser = argparse.ArgumentParser(description="Provide each word in a TEITOK document with a phonetic transcription")
+parser.add_argument("file", help="TEITOK file to transcribe")
+parser.add_argument("-l", "--lang", help="language of the file", type=str)
+parser.add_argument("--force", help="force when trancription exists", action="store_true")
+parser.add_argument("-s", "--sep", help="separator between phones", type=str, default=" ")
+parser.add_argument("-a", "--attr", help="attribute to your for transcription", type=str, default="phon")
+args = parser.parse_args()
+
+xmlf = etree.parse(args.file)
+# separate phones by a space and ignoring words boundaries
+separator = Separator(phone=args.sep, word=None)
+
+# initialize the espeak backend for English
+backend = EspeakBackend(args.lang)
+
+for tok in xmlf.iter('tok'):
+	word = tok.text
+	if not word: 
+		continue
+	phon = backend.phonemize([word], separator=separator, strip=True)[0].strip()
+	if phon and ( args.force or not args.attr in tok.attrib ):
+		tok.attrib[args.attr] = phon
+		
+xmlf.write(args.file,encoding="UTF-8")	
+print('Output written back to ' + args.file)
+	
