f026a831eea642059426a78330334813f95cef44
maartenpt@gmail.com <maartenpt@gmail.com>
2025-04-01 12:33:13 +0200

    improved tmx2teitok

--- Scripts/tmx2teitok.pl
+++ Scripts/tmx2teitok.pl
@@ -18,12 +18,17 @@ GetOptions ( ## Command line options
             'file=s' => \$filename, # filename to convert
             'output=s' => \$output, # filename for the converted file
             'morerev=s' => \$morerev, # additional revision nodes to put in the header
-            'split' => \$split, # Split into 1 file per language (default is to create a single XML with multiple languages)
+            'mode=s' => \$mode, # mode : join (one TEI xml file with different DIVs), split (one TEI file per language), annotate (TEI per language with @trans attribute)
+            'tuvelm=s' => \$tuvelm, # element to use for each tu (default: ab)
+            'tuid=s' => \$tuid, # element to use for the @tuid (default: tuid)
             );
 
 $\ = "\n"; $, = "\t";
 if ( !$filename ) { $filename = shift; };
 ( $basename = $filename ) =~ s/.*\///; $basename =~ s/\..*//;
+
+if ( !$tuvelm ) { $tuvelm = "ab"; };
+if ( !$tuid ) { $tuid = "tuid"; };
 
 $today = strftime "%Y-%m-%d", localtime;
 
@@ -38,17 +43,38 @@ foreach $tu ( $doc->findnodes("//tu") ) {
 	$appid = $basename.":tu-".$tunr++;
 	
 	foreach $tuv ( $tu->findnodes(".//tuv") ) {
-		$lang = $tuv->getAttribute('lang');
+		$lang = $tuv->getAttribute('xml:lang') or $lang = $tuv->getAttribute('lang');
 		$segtxt = $tuv->findnodes(".//seg")->item(0)->textContent;
-		$langparts{$lang} .= "\n<ab appid=\"$appid\">$segtxt</ab>";
+		if ( $mode eq 'annotate' ) {
+			$tseg{$lang} = $segtxt;
+			$ttr{$lang} = 1;
+		} else {
+			$langparts{$lang} .= "\n<$tuvelm $tuid=\"$appid\">$segtxt</$tuvelm>";
+		};
+	};
+	
+	if ( $mode eq 'annotate' ) {
+		while ( ($key, $val) = each ( %tseg ) ) { 
+			$trans = "";
+			$segtxt = $val;
+			while ( ($key2, $val2) = each ( %ttr ) ) { 
+				$val2 = $tseg{$key2};
+				if ( $val2 && $key ne $key2 ) { 
+					$val2 =~ s/\&/\&amp;/g; $val2 =~ s/"/\&#037;/g; $val2 =~ s/</\&lt;/g; $val2 =~ s/>/\&gt;/g;
+					$trans .= " trans_$key2=\"$val2\"";
+				};
+			};
+			$langparts{$key} .= "\n<$tuvelm $tuid=\"$appid\"$trans>$segtxt</$tuvelm>";
+		};
 	};
 	
 };
 
-if ( $split ) {
+if ( $mode eq 'split' || $mode eq 'annotate' ) {
+	$outbase = $output or $outbase = $filename;
 	while ( ( $key, $val) = each ( %langparts) ) {
-		( $output = $filename ) =~ s/\.tmx/-$key.xml/;
-		print "Writing converted file to $output\n";
+		( $output = $outbase ) =~ s/\.(tmx|xml)/-$key.xml/;
+		print "Writing converted file for $key to $output\n";
 		open OUTFILE, ">$output";
 		binmode(OUTFILE, ":utf8");
 		print OUTFILE "<TEI>\n<teiHeader>
